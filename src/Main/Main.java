cf0d39685142d2fb065da059c731384ffeaab2c4
Manhddm <manhducdongyb@gmail.com>
2025-03-15 16:41:37 +0700

    kkk

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,9 +10,13 @@ public class Main {
      * Phương thức main - điểm khởi đầu của chương trình
      * @param args Tham số dòng lệnh (không sử dụng)
      */
-    public static void main(String[] args) {
+    static {
+        System.out.println("Đang tải game...");
         System.out.println("Khởi động game...");
-        // Khởi tạo đối tượng Game - điều này sẽ bắt đầu vòng lặp game
+        //System.exit(0);
+    }   
+    public static void main(String[] args) {
+
         StartMenu st = new StartMenu();
     }
 }
